44c0d2af56e1dcc4efb9757337d4755f232b0d4f
unknown <nathaliamassis@gmail.com>
2020-03-31 10:48:04 -0300

    remoção de lixo

--- android/app/src/main/java/com/primeiroapp/MainActivity.java
+++ android/app/src/main/java/com/primeiroapp/MainActivity.java
@@ -1,9 +1,6 @@
 package com.primeiroapp;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,15 +11,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "primeiroApp";
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivotyDelegate(){
-    return new ReactActivityDelegate(this, getMainComponentName()){
-      @Override
-      protected ReactRootView createRootView(){
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    }
   }
 }
